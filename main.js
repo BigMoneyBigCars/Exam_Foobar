d3a991b43d2370292006e2e878faff9bb1ca599a
Lasse <L.stausgaard@gmail.com>
2020-05-20 00:16:21 +0200

    + / - works, full array done ready for push-method

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const popUp = document.querySelector("#product-details");
 const receipt = document.querySelector("#receipt");
 const receiptBut = document.querySelector(".receipt-button");
 
+//array
+let orderArray = [];
+
 function init() {
   console.log("tis ");
   popBut.addEventListener("click", () => {
@@ -51,18 +54,117 @@ function displayBeer(beer) {
   article = firstName + middleName + lastName;
 
   const clone = document.querySelector("template").cloneNode(true).content;
-  clone.querySelector("article").classList.add(article);
-  clone.querySelector("article").addEventListener("click", () => {
+
+  clone.querySelector("article").id = article;
+  clone.querySelector("article").dataset.name = beer.name;
+  clone.querySelector("article .desc-container").addEventListener("click", () => {
     showPopUp(beer);
   });
 
   clone.querySelector("h1").textContent = beer.name;
   clone.querySelector("p").textContent = "Alcohol " + beer.alc + "%";
+
+  clone.querySelector(".count").dataset.count = "0";
+
+  clone.querySelector(".fa-plus").addEventListener("click", () => {
+    plusBeer(article);
+  });
+  clone.querySelector(".fa-minus").addEventListener("click", () => {
+    minusBeer(article);
+  });
   console.log(beer.label);
   clone.querySelector(".img").style.backgroundImage = "url('/imgs/" + [beer.label] + "')";
   document.querySelector("#container").appendChild(clone);
 }
 
+// plus / minus beer
+
+function plusBeer(article) {
+  let beerCounter;
+
+  const element = document.querySelector("#" + [article]);
+  const elementCount = element.querySelector(".count");
+  const dataCount = element.querySelector(".count").dataset.count;
+
+  beerCounter = dataCount;
+  beerCounter++;
+
+  element.querySelector(".count").dataset.count = beerCounter;
+
+  elementCount.textContent = beerCounter;
+
+  // console.log(element.querySelector(".count").dataset);
+  addBeerArray(element.dataset.name, beerCounter);
+}
+
+function minusBeer(article) {
+  let beerCounter;
+  // console.log(beerCounter);
+  const element = document.querySelector("#" + [article]);
+  const elementCount = element.querySelector(".count");
+  const dataCount = element.querySelector(".count").dataset.count;
+  beerCounter = dataCount;
+  //console.log(beerCounter);
+
+  if (beerCounter > 0) {
+    beerCounter--;
+  }
+
+  // console.log(beerCounter);
+  element.querySelector(".count").dataset.count = beerCounter;
+  elementCount.textContent = beerCounter;
+  //console.log(element.querySelector(".count").dataset);
+
+  removeBeerArray(element.dataset.name, beerCounter);
+}
+
+function addBeerArray(beer, amount) {
+  //  orderArray[beer] = amount;
+  const obj = {
+    name: beer,
+    amount: amount,
+  };
+  const exists = orderArray.find((x) => x.name === beer);
+  console.log(exists);
+  if (exists) {
+    exists.amount = amount;
+  } else {
+    orderArray.push(obj);
+  }
+
+  console.log(orderArray);
+  orderedItems();
+}
+
+let currentlist = [];
+function removeBeerArray(beer, amount) {
+  console.log(amount);
+
+  const exists = orderArray.find((x) => x.name === beer);
+  if (exists) {
+    exists.amount = amount;
+  }
+
+  orderArray = orderArray.filter((x) => x.amount > 0);
+  console.log(orderArray);
+  /*   const index = orderArray.indexOf(beer);
+  if (index > -1) {
+    orderArray.splice(index, 1);
+  } */
+
+  orderedItems();
+}
+
+function orderedItems() {
+  let count = 0;
+
+  orderArray.forEach((obj) => {
+    count += obj.amount;
+  });
+  // console.log(count);
+
+  document.querySelector("#order .amount").textContent = "You are about to add " + [count] + " items";
+}
 // POPUP
 
 function showPopUp(beer) {
@@ -73,7 +175,6 @@ function showPopUp(beer) {
   popUp.querySelector(".type").textContent = beer.category;
   popUp.querySelector(".alc").textContent = beer.alc;
 
-  console.log(beer.description.aroma);
   popUp.querySelector(".impression p").textContent = beer.description.overallImpression;
   popUp.querySelector(".appearance p").textContent = beer.description.appearance;
   popUp.querySelector(".aroma p").textContent = beer.description.aroma;
@@ -87,6 +188,7 @@ function receiptEventlisterner() {
   console.log(receipt);
   receiptBut.addEventListener("click", () => {
     receipt.classList.toggle("active");
+    updateReceipt();
 
     /*   receipt.addEventListener("animationend", () => {
       receipt.classList.toggle("active");
@@ -100,4 +202,17 @@ function closeReceipt() {
   console.log(receipt);
 
   receipt.classList.remove("active");
+}
+
+function updateReceipt() {
+  document.querySelector("#receipt .wrapper").innerHTML = "";
+
+  orderArray.forEach((beer) => {
+    const receiptTemp = document.querySelector(".receipt-temp").cloneNode(true).content;
+    console.log(receiptTemp);
+
+    receiptTemp.querySelector(".item").textContent = beer.name;
+    receiptTemp.querySelector(".item-amount").textContent = beer.amount;
+    document.querySelector("#receipt .wrapper").appendChild(receiptTemp);
+  });
 }
